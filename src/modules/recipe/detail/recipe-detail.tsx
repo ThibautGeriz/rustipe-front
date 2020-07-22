4f7589d093784df3a47d572152de57cd969bc258
Thibaut Gery <thibaut.gery@gmail.com>
2020-07-22 23:14:12 +0200

    Add image in detail of recipe

--- src/modules/recipe/detail/recipe-detail.tsx
+++ src/modules/recipe/detail/recipe-detail.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import { StyleSheet, ScrollView } from 'react-native';
+import { StyleSheet, ScrollView, Image, Dimensions } from 'react-native';
 import { Paragraph, Surface, Text } from 'react-native-paper';
 
 import type { RecipeDetailProps } from './screen';
+
+const { width: viewWidth, height: viewHeight } = Dimensions.get('window');
 
 export default function RecipeDetail({ route }: RecipeDetailProps) {
   const { recipe } = route.params;
   return (
     <ScrollView style={styles.container}>
+      {recipe.imageUrl ? <Image style={styles.image} source={{ uri: recipe.imageUrl }} /> : null}
       <Surface style={styles.ingredients}>
         {recipe.ingredients.map((ingredient, index) => (
           // eslint-disable-next-line react/no-array-index-key
           <Text key={`ingredient${index}`} testID={`ingredient${index}`}>
-            -{ingredient}
+            - {ingredient}
           </Text>
         ))}
       </Surface>
       {recipe.instructions.map((instruction, index) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <Paragraph key={`instruction${index}`} testID={`instruction${index}`}>
-          {index + 1}.{instruction}
+        <Paragraph
+          style={styles.instruction}
+          // eslint-disable-next-line react/no-array-index-key
+          key={`instruction${index}`}
+          testID={`instruction${index}`}
+        >
+          {index + 1}. {instruction}
         </Paragraph>
       ))}
     </ScrollView>
@@ -29,10 +36,16 @@ export default function RecipeDetail({ route }: RecipeDetailProps) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    margin: 10,
   },
   ingredients: {
-    margin: 10,
+    margin: 20,
     padding: 5,
+  },
+  instruction: {
+    margin: 10,
+  },
+  image: {
+    width: viewWidth,
+    height: viewHeight / 3,
   },
 });

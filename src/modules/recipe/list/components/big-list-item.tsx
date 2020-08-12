084a7dc56941c12a7a22db35ead6518a228e2632
Thibaut Gery <thibaut.gery@gmail.com>
2020-08-12 16:33:31 +0200

    Use full card for big screen solves #9

--- src/modules/recipe/list/components/big-list-item.tsx
+++ src/modules/recipe/list/components/big-list-item.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
 import { Card, Chip, useTheme } from 'react-native-paper';
 import type Recipe from '../../models/recipe';
+
+const { width: viewWidth } = Dimensions.get('window');
 
 export interface RecipeListItemProps {
   recipe: Recipe;
@@ -13,31 +15,40 @@ export default function BigRecipeListItem({ recipe, onSelectRecipe }: RecipeList
   return (
     <Card testID={`Item-${recipe.id}`} style={styles.container} onPress={onSelectRecipe}>
       <Card.Cover source={{ uri: recipe.imageUrl }} />
-
       <Card.Title title={recipe.title} subtitle={recipe.description} />
       <Card.Content style={styles.cardContent}>
         <View style={styles.subContent}>
-          <Chip mode="outlined" style={styles.chip} icon="buffer">
-            {recipe.recipeYield}
-          </Chip>
-          <Chip mode="outlined" style={styles.chip} icon="knife">
-            {recipe.prepTimeInMinute}
-          </Chip>
-          <Chip mode="outlined" style={styles.chip} icon="toaster-oven">
-            {recipe.cookTimeInMinute}
-          </Chip>
+          {recipe.recipeYield && (
+            <Chip mode="outlined" style={styles.chip} icon="buffer">
+              {recipe.recipeYield}
+            </Chip>
+          )}
+          {recipe.prepTimeInMinute && (
+            <Chip mode="outlined" style={styles.chip} icon="knife">
+              {recipe.prepTimeInMinute}
+            </Chip>
+          )}
+          {recipe.cookTimeInMinute && (
+            <Chip mode="outlined" style={styles.chip} icon="toaster-oven">
+              {recipe.cookTimeInMinute}
+            </Chip>
+          )}
         </View>
         <View style={styles.subContent}>
-          <Chip mode="outlined" style={styles.chip}>
-            {recipe.cuisine}
-          </Chip>
-          <Chip
-            mode="outlined"
-            style={[styles.chip, { borderColor: colors.primary }]}
-            textStyle={{ color: colors.primary }}
-          >
-            {recipe.category}
-          </Chip>
+          {recipe.cuisine && (
+            <Chip mode="outlined" style={styles.chip}>
+              {recipe.cuisine}
+            </Chip>
+          )}
+          {recipe.category && (
+            <Chip
+              mode="outlined"
+              style={[styles.chip, { borderColor: colors.primary }]}
+              textStyle={{ color: colors.primary }}
+            >
+              {recipe.category}
+            </Chip>
+          )}
         </View>
       </Card.Content>
     </Card>
@@ -50,10 +61,12 @@ const styles = StyleSheet.create({
     marginRight: 5,
     marginLeft: 5,
     marginTop: 5,
+    width: (viewWidth - 10) / 3 - 10,
   },
   cardContent: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    flexWrap: 'wrap',
   },
   subContent: {
     flexDirection: 'row',

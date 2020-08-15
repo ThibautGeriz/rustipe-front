2e9450700aa8dfe1f611b7e5f4dff83dd4aefb25
Thibaut Gery <thibaut.gery@gmail.com>
2020-08-15 09:59:33 +0200

    Add logout solves #17

--- src/modules/recipe/list/header.tsx
+++ src/modules/recipe/list/header.tsx
@@ -1,10 +1,27 @@
 import * as React from 'react';
+import { AsyncStorage } from 'react-native';
 import { Appbar } from 'react-native-paper';
+import { useApolloClient } from '@apollo/client';
 
-const Header = () => (
-  <Appbar.Header>
-    <Appbar.Content title="Rustipe" />
-  </Appbar.Header>
-);
+import { AUTH_TOKEN_NAME } from '../../user/constants';
+import type { RecipeListProps } from './screen';
+
+const Header = ({ navigation }: RecipeListProps) => {
+  const client = useApolloClient();
+  return (
+    <Appbar.Header>
+      <Appbar.Content title="Rustipe" />
+      <Appbar.Action
+        icon="logout"
+        testID="logout"
+        onPress={async () => {
+          await AsyncStorage.removeItem(AUTH_TOKEN_NAME);
+          await client.clearStore();
+          navigation.navigate('Signin', {});
+        }}
+      />
+    </Appbar.Header>
+  );
+};
 
 export default Header;

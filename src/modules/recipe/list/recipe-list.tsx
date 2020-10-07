bff7838cb038eba1aba79a1f6b39a3e5852d3c76
Thibaut Gery <thibaut.gery@gmail.com>
2020-10-07 17:16:00 +0200

    Improve search screen experience

--- src/modules/recipe/list/recipe-list.tsx
+++ src/modules/recipe/list/recipe-list.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, FlatList, View } from 'react-native';
 import { useQuery } from '@apollo/client';
-import { Snackbar, FAB, useTheme, Portal, Searchbar } from 'react-native-paper';
+import { Snackbar, FAB, useTheme, Portal, Searchbar, ActivityIndicator } from 'react-native-paper';
 import { useMediaQuery } from 'react-responsive';
 
 import SmallItem from './components/small-list-item';
 import BigItem from './components/big-list-item';
 import ImportModalVisible from './components/import-modal';
 import NoRecipe from './components/no-recipe';
+import NoRecipeResult from './components/no-recipe-result';
 import type Recipe from '../models/recipe';
 import type { RecipeListProps } from './screen';
 import { GetMyRecipeData, GetMyRecipeVars, GET_MY_RECIPES } from './recipe-list-query';
@@ -36,7 +37,14 @@ export default function RecipeList({ navigation }: RecipeListProps) {
     error = undefined;
   };
   const Item = isMobile ? SmallItem : BigItem;
-
+  const recipies: Recipe[] = (data || {}).getMyRecipes || [];
+  let ListEmptyComponent = <NoRecipe navigation={navigation} />;
+  if (searchQuery) {
+    ListEmptyComponent = <NoRecipeResult />;
+  }
+  if (loading) {
+    ListEmptyComponent = <ActivityIndicator size="large" style={styles.activityIndicator} />;
+  }
   return (
     <View style={[styles.container, { backgroundColor: colors.background }]}>
       <Searchbar
@@ -48,12 +56,12 @@ export default function RecipeList({ navigation }: RecipeListProps) {
       />
 
       <FlatList
-        refreshing={loading}
+        refreshing={loading && recipies.length === 0}
         numColumns={isMobile ? 1 : 3}
         onRefresh={() => refetch({ query: searchQuery })}
-        ListEmptyComponent={<NoRecipe navigation={navigation} />}
+        ListEmptyComponent={ListEmptyComponent}
         testID="FlatList"
-        data={(data || {}).getMyRecipes || []}
+        data={recipies || []}
         renderItem={({ item: recipe }: any) => (
           <Item
             key={recipe.id}
@@ -102,6 +110,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  activityIndicator: {
+    margin: 50,
   },
   searchBox: {
     margin: 5,
